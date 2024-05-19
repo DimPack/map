da7083c488f661c3773edda5b1a8e67b8b0f22f5
Dima Prosto <csourgg@gmail.com>
2024-05-20 00:52:35 +0300

    finished the second task

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// 1) Створіть Map, де ключами будуть імена гравців, а значеннями - їхня кількість балів. Напишіть функції для додавання балів гравцям, віднімання балів і видалення гравців.
 const team = new Map();
 
 team.set('Emily','78');
@@ -63,3 +64,87 @@ function deletePlayer(map, player) {
 }
 
 console.log(deletePlayer(team, 'Ava'));
+
+// 2) За допомогою деструктуризації дістати з об'єкту слідуючі данні і покласти їх у змінні з відповідними іменами:
+// university, city, latitude, пошту з назвою userEmail, друге хобі з назвою secondHobbie, ім'я першої дитини з назвою firstChildName
+
+let user = {    
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    address: {
+        street: "123 Main Street",
+        city: "Anytown",
+        country: "USA",
+        postalCode: "12345",
+        state: "California",
+        coordinates: {
+            latitude: 37.7749,
+            longitude: -122.4194
+        }
+    },
+    contacts: {
+        email: "john.doe@example.com",
+        phone: "123-456-7890",
+        social: {
+            facebook: "john.doe.123",
+            twitter: "@johndoe",
+            instagram: "@johndoe123"
+        }
+    },
+    hobbies: ["reading", "traveling", "hiking"],
+    education: {
+        degree: "Bachelor's",
+        field: "Computer Science",
+        university: "University of California, Berkeley"
+    },
+    work: {
+        company: "ABC Corp",
+        position: "Software Engineer",
+        experience: "5 years"
+    },
+    family: {
+        spouse: {
+            firstName: "Jane",
+            lastName: "Doe",
+            age: 28
+        },
+        children: [
+            { name: "Emily", age: 5 },
+            { name: "Michael", age: 3 }
+        ]
+    }
+};
+const {
+education:{
+    university
+},
+address: {
+    city,
+    coordinates: {
+        latitude,
+    }
+},
+contacts:{
+    email
+},
+hobbies:[,secondHobi],
+family: {
+    children
+}
+} = user;
+
+
+const universityUser = university;
+const cityUser = city;
+const latitudeUser = latitude;
+const userEmail = email;
+const secondHobbie = secondHobi;
+const firstChildName = children[0].name;
+
+console.log(universityUser);
+console.log(cityUser);
+console.log(latitudeUser);
+console.log(userEmail);
+console.log(secondHobbie);
+console.log(firstChildName);
